040c9008cd223e15e28feab645b503beb40e3f75
Julien Da Silva <julien@lewagon.org>
2021-02-08 18:05:14 +0100

    ok

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,19 @@
+require "open-uri"
+
 class FlatsController < ApplicationController
+  before_action :load_flats
+
   def index
-    # => TODO fetch the flats infos
+  end
+
+  def show
+    @flat = @flats.find { |flat| flat['id'] == params[:id].to_i }
+  end
+
+  private
+
+  def load_flats
+    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    @flats = JSON.parse(open(url).read)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'flats#index', as: :root
   #   controller#action
+
+  get 'flats/:id', to: 'flats#show', as: :flat
+  # route parametric
 end
