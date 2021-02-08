3bb40132cedc4b90b009242070900c62feb7195a
Julien Da Silva <julien@lewagon.org>
2021-02-08 17:25:23 +0100

    Index of flats

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,5 @@
+class FlatsController < ApplicationController
+  def index
+    # => TODO fetch the flats infos
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  root to: 'flats#index', as: :root
+  #   controller#action
+end
